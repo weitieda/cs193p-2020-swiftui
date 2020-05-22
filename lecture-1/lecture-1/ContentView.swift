d7fce63a4148c5fc33677250b3d3f6a0355b32fe
Tieda Wei <weitieda@gmail.com>
2020-05-21 21:59:31 -0400

    Add lecture-1 starter

---
+++ lecture-1/lecture-1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  lecture-1
+//
+//  Created by Tieda Wei on 2020-05-21.
+//  Copyright © 2020 Tieda Wei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
