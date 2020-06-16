0ebe78f2d6ccf564efdc084b18398b6e0a2cb152
Tieda Wei <weitieda@gmail.com>
2020-06-15 22:23:26 -0400

    Started EmojiArt

---
+++ EmojiArt/EmojiArt/EmojiArtDocument.swift
@@ -0,0 +1,13 @@
+//
+//  EmojiArtDocument.swift
+//  EmojiArt
+//
+//  Created by Tieda Wei on 2020-06-15.
+//  Copyright © 2020 Tieda Wei. All rights reserved.
+//
+
+import SwiftUI
+
+class EmojiArtDocument: ObservableObject {
+    
+}
---
+++ EmojiArt/EmojiArt/EmojiArtDocumentView.swift
@@ -0,0 +1,18 @@
+//
+//  EmojiArtDocumentView.swift
+//  EmojiArt
+//
+//  Created by Tieda Wei on 2020-06-15.
+//  Copyright © 2020 Tieda Wei. All rights reserved.
+//
+
+import SwiftUI
+
+struct EmojiArtDocumentView: View {
+    
+    @ObservedObject var document: EmojiArtDocument
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
